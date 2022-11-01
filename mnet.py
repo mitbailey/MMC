f2f121b114e823a9571849771902be1d5d100b0b
mitbailey <mitbailey@outlook.com>
2022-10-31 20:13:52 -0400

    Added mnet

---
+++ mnet.py
@@ -0,0 +1,10 @@
+#
+# @file mnet.py
+# @author Mit Bailey (mitbailey@outlook.com)
+# @brief Universal MMC network communications protocol handler.
+# @version See Git tags for version information.
+# @date 2022.10.31
+# 
+# @copyright Copyright (c) 2022
+# 
+#
